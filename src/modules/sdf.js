38ac53cf058ee51630e5d2a64d54f0fec39ba74d
Andreas Gysin <andreas@ertdfgcvb.ch>
2020-08-29 11:28:14 +0200

    Add segment function

--- src/modules/sdf.js
+++ src/modules/sdf.js
@@ -5,7 +5,7 @@ https://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm
 */
 
 import { clamp, mix } from "./num.js"
-import { length } from "./vec2.js"
+import { length, sub, dot, mulN } from "./vec2.js"
 
 export function sdCircle(p, radius){ // vec2, float
 	return length(p) - radius
@@ -19,6 +19,13 @@ export function sdBox(p, size){     // vec2, vec2
 	d.x = Math.max(d.x, 0)
 	d.y = Math.max(d.y, 0)
 	return length(d) + Math.min(Math.max(d.x, d.y), 0.0)
+}
+
+export function sdSegment(p, a, b, thickness) {
+    const pa = sub(p, a)
+    const ba = sub(b, a)
+    const h = clamp(dot(pa, ba) / dot(ba, ba), 0.0, 1.0 )
+    return length(sub(pa, mulN(ba, h))) - thickness
 }
 
 export function opSmoothUnion( d1, d2, k ) {
@@ -35,3 +42,4 @@ export function opSmoothIntersection( d1, d2, k ) {
 	const h = clamp( 0.5 - 0.5 * (d2 - d1) / k, 0.0, 1.0 )
 	return mix( d2, d1, h ) + k * h * (1.0 - h)
 }
+
