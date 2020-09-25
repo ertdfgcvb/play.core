19938d9096eff7677eb4ef2b80641657aaa44236
Andreas Gysin <andreas@ertdfgcvb.ch>
2020-09-25 12:24:44 +0200

    Update list.sh

--- src/programs/list.sh
+++ src/programs/list.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Trims a string
+function trim {
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"
+    printf '%s' "$var"
+}
+
 # Script to generate a list of all the projects
 
 SCRIPT_PATH=${0%/*}    # path to this script
@@ -12,23 +22,23 @@ URL_PREFIX='/#/src'
 # Odered list of folders
 FOLDERS=(basics sdf demos camera contributed)
 
-
 for folder in ${FOLDERS[@]}; do
 
 	FIRST=1
 
 	for file in $SCRIPT_PATH/$folder/*.js; do
-
-		URL=$URL_PREFIX$(echo $file | sed -e 's/\.\///g' -e 's/\.js//g')
+		# The path in $file is the full path:
+		# ./play.core/src/programs/[folder]/[file].js
+		URL=$URL_PREFIX$(echo $file | sed -e 's/\.\///' -e 's/\.js//' -e 's/\.play\.core\/src\/programs//')
 		AUTHOR=$(sed -En 's/^@author[ \t](.*)$/\1/p' $file)
 		TITLE=$(sed -En 's/^@title[ \t](.*)$/\1/p' $file)
 		DESC=$(sed -En 's/^@desc[ \t](.*)$/\1/p' $file)
 		if [[ $FIRST == 1 ]]; then
-			FOLDER=$(echo $folder | sed -e 's/\.\///g' -e 's/\///g' -e 's/\.js//g' )
+			FOLDER=$(echo $folder | sed -e 's/\.\///' -e 's/\///' -e 's/\.js//' )
 			FIRST=0
 			printf "\t"
 			printf "<div>"
-			printf "$FOLDER"
+			printf "$(trim $FOLDER)"
 			printf "</div>"
 			printf "\n"
 		else
@@ -40,13 +50,13 @@ for folder in ${FOLDERS[@]}; do
 
 		printf "\t"
 		printf "<div>"
-		printf "<a target='_blank' href='$URL'>$TITLE</a>"
+		printf "<a target='_blank' href='$URL'>$(trim $TITLE)</a>"
 		printf "</div>"
 		printf "\n"
 
 		printf "\t"
 		printf "<div>"
-		printf "$DESC"
+		printf "$(trim $DESC)"
 		printf "</div>"
 		printf "\n"
 
