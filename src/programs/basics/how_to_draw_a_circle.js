784f18c943051495695581d911e73c703a3a9791
Andreas Gysin <andreas@ertdfgcvb.ch>
2021-01-06 12:21:10 +0100

    Update how_to_draw_a_circle.js

--- src/programs/basics/how_to_draw_a_circle.js
+++ src/programs/basics/how_to_draw_a_circle.js
@@ -9,27 +9,28 @@ import { length } from '/src/modules/vec2.js'
 
 export function main(coord, context, cursor, buffer) {
 
-	// Used to fix screen aspect
-	const m = Math.min(context.cols, context.rows)
-
-    // contex.aspect contains the cell aspect ratio
+    // contex.metrics.aspect contains the font (or cell) aspect ratio
 	const aspectRatio = cursor.pressed ? context.metrics.aspect : 1
 
-	// Coordinate space (-1, 1) & corrected screen aspect (a)
+	// Transform coordinate space to (-1, 1)
+	// width corrected screen aspect (m) and cell aspect (aspectRatio)
+    const m = Math.min(context.cols, context.rows)
     const st = {
         x : 2.0 * (coord.x - context.cols / 2) / m * aspectRatio, // apply aspect
         y : 2.0 * (coord.y - context.rows / 2) / m
     }
 
-	// Distance of point from center
+	// Distance of each cell from the center (0, 0)
 	const l = length(st)
+
+	// 0.6 is the radius of the circle
     return l < 0.6  ? 'X' : '.'
 }
 
 // Draw some info
 import { drawBox } from '/src/modules/drawbox.js'
 export function post(context, cursor, buffer) {
-	// Apply some rounding
+	// Apply some rounding to the aspect for better output
 	const ar = cursor.pressed ? (''+context.metrics.aspect).substr(0, 8) : 1
 
 	// Output string
