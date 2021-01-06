3639b4d5af0b0bf3be4b8eaf5450f4daf1fd8ab5
Andreas Gysin <andreas@ertdfgcvb.ch>
2021-01-06 12:12:22 +0100

    Update time_frames.js

--- src/programs/basics/time_frames.js
+++ src/programs/basics/time_frames.js
@@ -1,19 +1,23 @@
 /**
 @author ertdfgcvb
 @title  Time: frames
-@desc   Use of context.frame
+@desc   Use of context.frame (ASCII horizon)
 [header]
 */
 
 // The default framerate can be altered
 // by exporting a 'settings' object (see the manual for details).
-export const settings = { fps : 10 }
+export const settings = { fps : 30 }
 
 export function main(coord, context, cursor, buffer) {
-	const f = context.frame
-	return String.fromCharCode((coord.y + coord.x + f) % 32 + 65)
+	const z = Math.floor((coord.y - context.rows / 2))
+	if (z == 0) return ' '
+
+	const code = Math.floor((coord.x - context.cols/2) / z + context.frame	* 0.3)
+	return String.fromCharCode(code % 94 + 32)
 }
 
+// Display some info
 import { drawInfo } from '/src/modules/drawbox.js'
 export function post(context, cursor, buffer) {
 	drawInfo(context, cursor, buffer, {
