4d1809e12bcb1e735b2c3b26c4f0f33aee3ea1ce
Andreas Gysin <andreas@ertdfgcvb.ch>
2021-01-06 11:30:26 +0100

    Simpler default example

--- src/programs/basics/coordinates_xy.js
+++ src/programs/basics/coordinates_xy.js
@@ -5,8 +5,11 @@
 [header]
 */
 
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ.:!?'
+
 export function main(coord, context, cursor, buffer) {
 	// To generate an output return a character
 	// or an object with a “char” field, for example {char: 'x'}
-	return String.fromCharCode((coord.y + coord.x) % 62 + 65)
+	const i = (coord.y + coord.x) % chars.length
+	return chars[i]
 }
