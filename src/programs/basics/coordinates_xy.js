9298efa46618a42cf538e1dcd4d9bd8b6684f21b
Andreas Gysin <andreas@ertdfgcvb.ch>
2021-01-06 12:12:20 +0100

    Update coordinates_xy.js

--- src/programs/basics/coordinates_xy.js
+++ src/programs/basics/coordinates_xy.js
@@ -10,6 +10,6 @@ const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ.:!?'
 export function main(coord, context, cursor, buffer) {
 	// To generate an output return a character
 	// or an object with a “char” field, for example {char: 'x'}
-	const i = (coord.y + coord.x) % chars.length
+	const i = (coord.y + coord.x + context.frame) % chars.length
 	return chars[i]
 }
