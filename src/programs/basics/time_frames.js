08637121dae7fab3f25511e7fbfd8505faef1377
Andreas Gysin <andreas@ertdfgcvb.ch>
2021-01-06 12:36:26 +0100

    Update time_frames.js

--- src/programs/basics/time_frames.js
+++ src/programs/basics/time_frames.js
@@ -11,10 +11,17 @@ export const settings = { fps : 30 }
 
 export function main(coord, context, cursor, buffer) {
 	const z = Math.floor((coord.y - context.rows / 2))
+
+	// Avoid division by zero
 	if (z == 0) return ' '
 
-	const code = Math.floor((coord.x - context.cols/2) / z + context.frame	* 0.3)
-	return String.fromCharCode(code % 94 + 32)
+	// Calculate a fake perspective
+	const val = (coord.x - context.cols/2) / z
+
+	// Add time (context.frame) for animation
+	// and make sure to get adisplayable charCode (int, positive, valid range)
+	const code = Math.floor(val + context.cols/2 + context.frame * 0.3) % 94 + 32
+	return String.fromCharCode(code)
 }
 
 // Display some info
