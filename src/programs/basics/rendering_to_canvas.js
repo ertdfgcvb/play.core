1b4be2fbb8d7d2311715c184515e45ed3f2b0b6b
Andreas Gysin <andreas@ertdfgcvb.ch>
2021-01-08 11:49:48 +0100

    Create rendering_to_canvas.js

---
+++ src/programs/basics/rendering_to_canvas.js
@@ -0,0 +1,37 @@
+/**
+@author ertdfgcvb
+@title  Canvas renderer
+@desc   Rendering to a canvas element
+[header]
+*/
+
+// A few extra fields are available when choosing the canvas renderer:
+// The offset (from top, left) and the size of the canvas element.
+export const settings = {
+	renderer : 'canvas',
+	// Settings available only
+	// for the 'canvas' renderer
+	canvasOffset : {
+		x : 'auto',
+		y : 20
+	},
+	canvasSize : {
+		width : 400,
+		height : 500
+	},
+	// Universal settings
+	cols : 42,
+	rows : 22,
+	background : 'pink',
+	color : 'black'
+}
+
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ.:!?'
+
+export function main(coord, context, cursor, buffer) {
+	const {x, y} = coord
+	const f = context.frame
+	const l = chars.length
+	const c = context.cols
+	return y % 2 ? chars[(y + x + f) % l] : chars[(y + c - x + f) % l]
+}
