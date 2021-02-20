436a642cdba3c41133474698f48847ed1b15fd23
Andreas Gysin <andreas@ertdfgcvb.ch>
2021-02-20 14:35:46 +0100

    Update coordinates_index.js

--- src/programs/basics/coordinates_index.js
+++ src/programs/basics/coordinates_index.js
@@ -6,7 +6,8 @@
 */
 
 // Global variables have scope in the whole module.
-const pattern = '| |▁|▂|▃|▄|▅|▆|▇|▆|▅|▄|▃|▂|▁'
+const pattern = '| |.|,|:|;|x|K|Ñ|R|a|+|=|-|_'
+// const pattern = '| |▁|▂|▃|▄|▅|▆|▇|▆|▅|▄|▃|▂|▁'
 
 // Resize the browser window to modify the pattern.
 export function main(coord, context, cursor, buffer) {
