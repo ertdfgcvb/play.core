177cd476dd9c8dea7c85b42e87983d2d950c5a94
Andreas Gysin <andreas@ertdfgcvb.ch>
2022-04-03 14:20:23 +0200

    Update coordinates_xy.js

--- src/programs/basics/coordinates_xy.js
+++ src/programs/basics/coordinates_xy.js
@@ -11,14 +11,13 @@ export function main(coord, context, cursor, buffer) {
 	// To generate an output return a single character
 	// or an object with a “char” field, for example {char: 'x'}
 
-	// Shortcuts for frame and coord
-	const f = context.frame
+	// Shortcuts for frame, cols and coord (x, y)
+	const {cols, frame } = context
 	const {x, y} = coord
 
-	// Invert direction for odd rows
-	if (coord.y % 2 == 0) {
-		return density[(y + x + f) % density.length]
-	} else {
-		return density[(y + context.cols - x + f) % density.length]
-	}
+	// -1 for even lines, 1 for odd lines
+	const sign = y % 2 * 2 - 1
+	const index = (cols + y + x * sign + frame) % density.length
+
+	return density[index]
 }
