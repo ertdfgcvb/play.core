5f35e06715f96d3105e4ff7279defdbaea1b7621
Andreas Gysin <andreas@ertdfgcvb.ch>
2023-01-31 23:19:50 +0100

    Create 10print.js

---
+++ src/programs/basics/10print.js
@@ -0,0 +1,20 @@
+/**
+[header]
+@author ertdfgcvb
+@title  10 PRINT
+@desc   10 PRINT CHR$(205.5+RND(1)); : GOTO 10
+See also:
+https://10print.org
+*/
+
+// Run the program only once
+export const settings = {
+	once : true
+}
+
+export function main() {
+	// Also try: ╩ ╦ or ▄ ░
+	// or any combination from
+	// https://play.ertdfgcvb.xyz/abc.html#font:characterset
+	return Math.random() < 0.5 ? '╱' : '╲'
+}
